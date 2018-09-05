f8e22efeaa3cca75793377072a19dca66a6f91f3
Prodipta Guha <pguha@student.unimelb.edu.au>
2018-09-05 21:28:46 +1000

    create homepage

--- app/src/main/java/coolgroup/com/aline/MainActivity.java
+++ app/src/main/java/coolgroup/com/aline/MainActivity.java
@@ -1,13 +1,27 @@
 package coolgroup.com.aline;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    Button btnSignIn, btnRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Init View
+        btnRegister = findViewById(R.id.btnRegister);
+        btnSignIn = findViewById(R.id.btnSignIn);
     }
 }
