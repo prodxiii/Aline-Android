fcc93874773c281a5c4d254b5512b2ea5550d153
Tristan Cliff <tcliff@student.unimelb.edu.au>
2018-09-14 10:23:55 +1000

    Added a Firebase Communicator to Controller
    Signed-off-by: mrcliffster <tcliff@student.unimelb.edu.au>

--- app/src/main/java/coolgroup/com/aline/Controller.java
+++ app/src/main/java/coolgroup/com/aline/Controller.java
@@ -1,9 +1,15 @@
 package coolgroup.com.aline;
 
+import coolgroup.com.aline.Model.FirebaseCommunicator;
+import coolgroup.com.aline.Model.iServerCommunicator;
+
 public class Controller {
+
     /* Follows Singleton design pattern */
     private static Controller instance = new Controller();
 
+    // Controlled classes
+    public iServerCommunicator serverCommunicator = new FirebaseCommunicator(); // Not sure if this should be of type iServerCommunicator or FirebaseCommunicator?
     public Controller() {
         return;
     }
