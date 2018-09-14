d9b8bb37e87ced7ea17b92ea303865d806e7e5e3
Tristan Cliff <tcliff@student.unimelb.edu.au>
2018-09-14 10:18:49 +1000

    Added Javadoc to FirebaseCommunicator
    Signed-off-by: mrcliffster <tcliff@student.unimelb.edu.au>

--- app/src/main/java/coolgroup/com/aline/Model/FirebaseCommunicator.java
+++ app/src/main/java/coolgroup/com/aline/Model/FirebaseCommunicator.java
@@ -1,4 +1,103 @@
 package coolgroup.com.aline.Model;
 
+import java.util.ArrayList;
+
 public class FirebaseCommunicator implements iServerCommunicator {
+    /**
+     * Authenticate a user by email address and password.
+     *
+     * @param email    The email registered to the account.
+     * @param password The user’s password.
+     * @return True if the account exists and details are correct, else false.
+     */
+    @Override
+    public boolean logInUserEmail(String email, String password) {
+        return false;
+    }
+
+    /**
+     * Authenticate a user by phone number and password.
+     *
+     * @param phone    The phone number registered to the account.
+     * @param password The user’s password.
+     * @return True if the account exists and details are correct, else false.
+     */
+    @Override
+    public boolean logInUserPhone(String phone, String password) {
+        return false;
+    }
+
+    /**
+     * Register an account for a user.
+     *
+     * @param email    The user’s email address.
+     * @param password The user’s selected password.
+     * @param name     The user’s full name.
+     * @param phone    The user’s phone number.
+     * @return True if the user doesn’t already exist (e.g. email taken)
+     * and the format of all arguments is valid (e.g. password length).
+     */
+    @Override
+    public boolean signUpUser(String email, String password, String name, String phone) {
+        return false;
+    }
+
+    /**
+     * Retrieve a user ID string.
+     *
+     * @param email The email of the user to be queried.
+     * @param name  The name of the user to be queried.
+     * @param phone The phone of the user to be queried.
+     * @return The user ID if the user exists, else null.
+     */
+    @Override
+    public String getUserId(String email, String name, String phone) {
+        return null;
+    }
+
+    /**
+     * Retrieve the basic details of a user.
+     *
+     * @param userId The user ID of the user to be queried.
+     * @return A User object corresponding to the user, if it exists (else null).
+     */
+    @Override
+    public User getBasicUserInfo(String userId) {
+        return null;
+    }
+
+    /**
+     * Retrieve all contacts of a user.
+     *
+     * @param userId The user to be queried.
+     * @return An ArrayList of the user’s contacts.
+     */
+    @Override
+    public ArrayList<String> getContactsList(String userId) {
+        return null;
+    }
+
+    /**
+     * Add a new user to the list of contacts.
+     *
+     * @param userId        The user whose contact list is being updated.
+     * @param contactUserId The user to be added.
+     * @return True if the update was successful.
+     */
+    @Override
+    public boolean addNewContact(String userId, String contactUserId) {
+        return false;
+    }
+
+    /**
+     * Remove a user from the list of contacts.
+     *
+     * @param userId        The user whose contact list is being updated.
+     * @param contactUserId The user to be removed.
+     * @return True if the removal was successful.
+     */
+    @Override
+    public boolean removeContact(String userId, String contactUserId) {
+        return false;
+    }
 }
