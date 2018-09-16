2267ceaca4163b099ceecd873c1688364e61736b
Tristan Cliff <tcliff@student.unimelb.edu.au>
2018-09-16 16:17:16 +1000

    Added a ContactsList Activity, and made it navigateable.
    Signed-off-by: mrcliffster <tcliff@student.unimelb.edu.au>

---
+++ app/src/main/java/coolgroup/com/aline/View/ContactsList.java
@@ -0,0 +1,31 @@
+package coolgroup.com.aline.View;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import coolgroup.com.aline.R;
+
+public class ContactsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
