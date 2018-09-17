94e4c8fe09388c8d599dec3e7c0346d8d124ba47
revolvolutionary <legoasaurus@gmail.com>
2018-09-17 20:42:32 +1000

    added sheel of test file

---
+++ app/src/test/java/coolgroup/com/aline/FirebaseCommunicatorTest.java
@@ -0,0 +1,26 @@
+package coolgroup.com.aline;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FirebaseCommunicatorTest {
+
+    @Mock
+    Context mMockContext;
+
+    public void testValidEmailLogin() {
+
+        // Mockito initialisation
+        //when(mMockContext.)
+
+    }
+}
