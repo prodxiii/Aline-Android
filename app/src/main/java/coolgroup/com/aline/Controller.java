923350e1468ffe87a542fa0f47c628799bf50e63
Alan Ung <alanu@student.unimelb.edu.au>
2018-09-17 16:10:31 +1000

    Add serverCommunicator and mainUser to Controller

--- app/src/main/java/coolgroup/com/aline/Controller.java
+++ app/src/main/java/coolgroup/com/aline/Controller.java
@@ -1,14 +1,41 @@
 package coolgroup.com.aline;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import coolgroup.com.aline.Model.FirebaseCommunicator;
+import coolgroup.com.aline.Model.User;
+import coolgroup.com.aline.Model.iServerCommunicator;
+
 public class Controller {
+
     /* Follows Singleton design pattern */
     private static Controller instance = new Controller();
 
-    public Controller() {
-        return;
+    private final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    public final iServerCommunicator serverCommunicator = new FirebaseCommunicator(db);
+
+    private User mainUser;
+
+    public Controller() {}
+
+    /**
+     * Initialise the controller with a signed in user.
+     * @param user The current signed in user.
+     */
+    public Controller(User user) {
+        mainUser = user;
     }
 
     public static Controller getInstance() {
         return instance;
     }
+
+    public User getMainUser() {
+        return mainUser;
+    }
+
+    public void setMainUser(User user) {
+        mainUser = user;
+    }
+
 }
