c6d64e9e7a33f85f5fc9abbbace4e8fa147ea2b8
Prodipta Guha <pguha@student.unimelb.edu.au>
2018-09-28 13:42:58 +1000

    create appbar for chat activity

--- app/src/main/java/coolgroup/com/aline/view/Chat.java
+++ app/src/main/java/coolgroup/com/aline/view/Chat.java
@@ -1,8 +1,13 @@
 package coolgroup.com.aline.view;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -19,6 +24,11 @@ public class Chat extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         mAuth = FirebaseAuth.getInstance();
+
+        // Create the toolbar for the chat activity
+        Toolbar mToolbar = findViewById(R.id.chatAppBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("This is chat activity");
     }
 
     @Override
@@ -28,16 +38,47 @@ public class Chat extends AppCompatActivity {
         // Get the current user ID
         FirebaseUser currentUser = mAuth.getCurrentUser();
 
+        // User is not signed in
         if (currentUser == null) {
             Intent authIntent = new Intent(Chat.this, Authenticate.class);
 
-            // Validation to stop user from going to the authenticate activity again
             authIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
             // Take user to authentication
             startActivity(authIntent);
             finish();
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if (item.getItemId() == R.id.menuSignOut) {
+            FirebaseAuth.getInstance().signOut();
+            sendToAuth();
+        }
+
+        return true;
+    }
+
+    private void sendToAuth() {
+        Intent authIntent = new Intent(Chat.this, Authenticate.class);
+
+        // Validation to stop user from going to the authenticate activity again
+        authIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        // Take user to authentication
+        startActivity(authIntent);
+        finish();
     }
 }
--- app/src/main/java/coolgroup/com/aline/view/Register.java
+++
@@ -1,4 +0,0 @@
-package coolgroup.com.aline.view;
-
-public class Register {
-}
