77e4fd857826cec817ecc16f9e2c1e244ef00a35
Prodipta Guha <pguha@student.unimelb.edu.au>
2018-10-11 00:53:46 +1100

    edit account settings activity

--- app/src/main/java/coolgroup/com/aline/view/options/AccountSettingsActivity.java
+++ app/src/main/java/coolgroup/com/aline/view/options/AccountSettingsActivity.java
@@ -1,120 +1,311 @@
 package coolgroup.com.aline.view.options;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 import coolgroup.com.aline.R;
 import coolgroup.com.aline.view.update.StatusActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
+import id.zelory.compressor.Compressor;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
-    //**************FIREBASE******************//
     private DatabaseReference mUserDatabase;
     private FirebaseUser mCurrentUser;
-    //**************FIREBASE******************//
 
-    // Account Settings Layout
-    private CircleImageView mAccountImage;
-    private TextView mAccountName;
-    private TextView mAccountStatus;
-    private TextView mAccountPhone;
-    private TextView mAccountEmail;
 
-    private Button mChangeImage;
-    private Button mUpdateStatus;
+    //Android Layout
 
-    private Toolbar mToolbar;
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
+
+    private Button mStatusBtn;
+    private Button mImageBtn;
+
 
     private static final int GALLERY_PICK = 1;
+
+    // Storage Firebase
+    private StorageReference mImageStorage;
+
+    private ProgressDialog mProgressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
 
-        mAccountImage = (CircleImageView) findViewById(R.id.account_image);
-        mAccountName = (TextView) findViewById(R.id.account_name);
-        mAccountStatus = (TextView) findViewById(R.id.account_status);
-        mAccountEmail = (TextView) findViewById(R.id.account_email);
-        mAccountPhone = (TextView) findViewById(R.id.account_phone);
+        mDisplayImage = (CircleImageView) findViewById(R.id.settings_image);
+        mName = (TextView) findViewById(R.id.settings_name);
+        mStatus = (TextView) findViewById(R.id.settings_status);
 
-        mChangeImage = (Button) findViewById(R.id.btn_change_image);
-        mUpdateStatus = (Button) findViewById(R.id.btn_update_status);
+        mStatusBtn = (Button) findViewById(R.id.settings_status_btn);
+        mImageBtn = (Button) findViewById(R.id.settings_image_btn);
 
-        mToolbar = (Toolbar) findViewById(R.id.profile_appbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setTitle("Account Settings");
+        mImageStorage = FirebaseStorage.getInstance().getReference();
 
-        //**************FIREBASE******************//
         mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         String current_uid = mCurrentUser.getUid();
 
+
+
         mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
-        mUserDatabase.keepSynced(true); // Keep offline
-        //***************************************//
+        mUserDatabase.keepSynced(true);
 
         mUserDatabase.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot) {
 
-                if (dataSnapshot.exists()) {
-                    String name = dataSnapshot.child("name").getValue().toString();
-                    String image = dataSnapshot.child("image").getValue().toString();
-                    String status = dataSnapshot.child("status").getValue().toString();
-                    String email = dataSnapshot.child("email").getValue().toString();
-                    String phone = dataSnapshot.child("phone").getValue().toString();
-                    String thumbnail = dataSnapshot.child("thumbnail").getValue().toString();
+                String name = dataSnapshot.child("name").getValue().toString();
+                final String image = dataSnapshot.child("image").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
 
-                    mAccountName.setText(name);
-                    mAccountStatus.setText(status);
-                    mAccountEmail.setText(email);
-                    mAccountPhone.setText(phone);
+                mName.setText(name);
+                mStatus.setText(status);
+
+                if(!image.equals("default")) {
+
+                    //Picasso.with(SettingsActivity.this).load(image).placeholder(R.drawable.default_avatar).into(mDisplayImage);
+
+                    Picasso.with(AccountSettingsActivity.this).load(image).networkPolicy(NetworkPolicy.OFFLINE)
+                            .placeholder(R.drawable.avatar_male).into(mDisplayImage, new Callback() {
+                        @Override
+                        public void onSuccess() {
+
+                        }
+
+                        @Override
+                        public void onError() {
+
+                            Picasso.with(AccountSettingsActivity.this).load(image).placeholder(R.drawable.avatar_male).into(mDisplayImage);
+
+                        }
+                    });
+
                 }
+
+
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
-        mUpdateStatus.setOnClickListener(v -> {
 
-            String statusValue = mAccountStatus.getText().toString();
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-            Intent statusIntent = new Intent(AccountSettingsActivity.this, StatusActivity.class);
-            statusIntent.putExtra("statusValue", statusValue);
+                String status_value = mStatus.getText().toString();
 
-            startActivity(statusIntent);
+                Intent status_intent = new Intent(AccountSettingsActivity.this, StatusActivity.class);
+                status_intent.putExtra("status_value", status_value);
+                startActivity(status_intent);
+
+            }
         });
 
-        // TODO: 29/9/18 Incomplete. Ref: Picasso, CropImage and Part 13
-        mChangeImage.setOnClickListener(v -> {
 
-            Intent galleryIntent = new Intent();
-            galleryIntent.setType("image/*");
-            galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-            startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
 
+                Intent galleryIntent = new Intent();
+                galleryIntent.setType("image/*");
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(galleryIntent, "SELECT IMAGE"), GALLERY_PICK);
+
+
+                /*
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(SettingsActivity.this);
+                        */
+
+            }
         });
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+            Uri imageUri = data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1, 1)
+                    .setMinCropWindowSize(500, 500)
+                    .start(this);
+
+            //Toast.makeText(SettingsActivity.this, imageUri, Toast.LENGTH_LONG).show();
+
+        }
+
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+
+            if (resultCode == RESULT_OK) {
+
+
+                mProgressDialog = new ProgressDialog(AccountSettingsActivity.this);
+                mProgressDialog.setTitle("Uploading Image...");
+                mProgressDialog.setMessage("Please wait while we upload and process the image.");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+
+                Uri resultUri = result.getUri();
+
+                File thumb_filePath = new File(resultUri.getPath());
+
+                String current_user_id = mCurrentUser.getUid();
+
+
+                Bitmap thumb_bitmap = new Compressor(this)
+                        .setMaxWidth(200)
+                        .setMaxHeight(200)
+                        .setQuality(75)
+                        .compressToBitmap(thumb_filePath);
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                thumb_bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                final byte[] thumb_byte = baos.toByteArray();
+
+
+                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
+                final StorageReference thumb_filepath = mImageStorage.child("profile_images").child("thumbs").child(current_user_id + ".jpg");
+
+
+
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                        if(task.isSuccessful()){
+
+                            final String download_url = task.getResult().getDownloadUrl().toString();
+
+                            UploadTask uploadTask = thumb_filepath.putBytes(thumb_byte);
+                            uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> thumb_task) {
+
+                                    String thumb_downloadUrl = thumb_task.getResult().getDownloadUrl().toString();
+
+                                    if(thumb_task.isSuccessful()){
+
+                                        Map update_hashMap = new HashMap();
+                                        update_hashMap.put("image", download_url);
+                                        update_hashMap.put("thumb_image", thumb_downloadUrl);
+
+                                        mUserDatabase.updateChildren(update_hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                if(task.isSuccessful()){
+
+                                                    mProgressDialog.dismiss();
+                                                    Toast.makeText(AccountSettingsActivity.this, "Success Uploading.", Toast.LENGTH_LONG).show();
+
+                                                }
+
+                                            }
+                                        });
+
+
+                                    } else {
+
+                                        Toast.makeText(AccountSettingsActivity.this, "Error in uploading thumbnail.", Toast.LENGTH_LONG).show();
+                                        mProgressDialog.dismiss();
+
+                                    }
+
+
+                                }
+                            });
+
+
+
+                        } else {
+
+                            Toast.makeText(AccountSettingsActivity.this, "Error in uploading.", Toast.LENGTH_LONG).show();
+                            mProgressDialog.dismiss();
+
+                        }
+
+                    }
+                });
+
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+
+                Exception error = result.getError();
+
+            }
+        }
+
+
+    }
+
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(20);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+
 }
